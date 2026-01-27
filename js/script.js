8aec79e8c7ab29e9d374289413b5882d3b5dee42
maidire3 <lookcart123@gmail.com>
2026-01-27 18:18:34 +0100

    Riorganizzazione pulsanti e miglioramento dello stile CSS

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const creditiInizialiSquadre = {
   "Aston Villain": 0,
   "Bayer Leverkubo": 7,
   "FC Kame House": 0,
-  "GiocatoriSempreCaxxuti": 11,
+  "GiocatoriSempreCazzuti": 11,
   "One Pisa": 18,
   "Real Madrink": 2,
   "Woolferhampton": 3,
@@ -176,7 +176,7 @@ const defaultPlayers = {
     { nome: "Castellanos*", crediti: 141, ruolo: "attaccanti" },
     { nome: "Dzeko*", crediti: 2, ruolo: "attaccanti" }
   ],
-  "GiocatoriSempreCaxxuti": [],
+  "GiocatoriSempreCazzuti": [],
   "One Pisa": [
     {nome: "stanciu*", crediti: 9, ruolo: "centrocampisti"},
     {nome: "Lucca*", crediti: 10, ruolo: "attaccanti"}
@@ -190,9 +190,298 @@ const defaultPlayers = {
   "Ovo al tegamino": [{nome: "Carboni V.*", crediti: 6, ruolo:"centrocampisti"}]
 };
 
-function inserisciDefaultPlayers() {
-  btnDefault.style.display="none";
-  for (const nomeSquadra in defaultPlayers) {
+const primoSave = {
+  "AC Orazkhelia": [
+    {
+      "nome": "Turati",
+      "crediti": "1",
+      "ruolo": "portieri"
+    },
+    {
+      "nome": "Gatti",
+      "crediti": "7",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Zhegrova",
+      "crediti": "34",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Collocolo",
+      "crediti": "3",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Bravo*",
+      "crediti": 1,
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Ngonge",
+      "crediti": "11",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "Aston Villain": [
+    {
+      "nome": "Martinez Jo",
+      "crediti": "1",
+      "ruolo": "portieri"
+    },
+    {
+      "nome": "Tavares",
+      "crediti": "30",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Nicolussi Caviglia",
+      "crediti": "1",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Gimenez",
+      "crediti": "30",
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Vlahovic",
+      "crediti": "40",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "Bayer Leverkubo": [
+    {
+      "nome": "Biraghi",
+      "crediti": "5",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Dele-Bashiru",
+      "crediti": "11",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Folorunsho",
+      "crediti": "8",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Belotti",
+      "crediti": "18",
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Soulemana",
+      "crediti": "1",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "FC Kame House": [
+    {
+      "nome": "Martinelli T.*",
+      "crediti": 1,
+      "ruolo": "portieri"
+    },
+    {
+      "nome": "Angelino",
+      "crediti": "32",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Guendouzi*",
+      "crediti": 1,
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Castellanos*",
+      "crediti": 141,
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Dzeko*",
+      "crediti": 2,
+      "ruolo": "attaccanti"
+    }
+  ],
+    "GiocatoriSempreCazzuti": [
+    {
+      "nome": "Floriani Mussolini",
+      "crediti": "4",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Sohm",
+      "crediti": "4",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Camarda",
+      "crediti": "12",
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Cutrone",
+      "crediti": "1",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "One Pisa": [
+    {
+      "nome": "Siegrist",
+      "crediti": "1",
+      "ruolo": "portieri"
+    },
+    {
+      "nome": "Olivera",
+      "crediti": "2",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "stanciu*",
+      "crediti": 9,
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Maldini",
+      "crediti": "6",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Lucca*",
+      "crediti": 10,
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Ferguson",
+      "crediti": "125",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "Real Madrink": [
+    {
+      "nome": "Kossonou",
+      "crediti": "2",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Helgason",
+      "crediti": "1",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Venturino",
+      "crediti": "1",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Stengs",
+      "crediti": "6",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Lang*",
+      "crediti": 1,
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Dovbyk",
+      "crediti": "36",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "Woolferhampton": [
+    {
+      "nome": "Lovik*",
+      "crediti": 1,
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Vazquez*",
+      "crediti": 11,
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "El Aynaoui",
+      "crediti": "3",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Gronbaek",
+      "crediti": "7",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Neres",
+      "crediti": "6",
+      "ruolo": "centrocampisti"
+    }
+  ],
+  "Yara FC": [
+    {
+      "nome": "Israel",
+      "crediti": "13",
+      "ruolo": "portieri"
+    },
+    {
+      "nome": "Cuadrado",
+      "crediti": "1",
+      "ruolo": "difensori"
+    },
+    {
+      "nome": "Bailey*",
+      "crediti": 11,
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "De Bruyne",
+      "crediti": "90",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Nzola",
+      "crediti": "13",
+      "ruolo": "attaccanti"
+    },
+    {
+      "nome": "Diao",
+      "crediti": "1",
+      "ruolo": "attaccanti"
+    }
+  ],
+  "Ovo al tegamino": [
+    {
+      "nome": "Mandas*",
+      "crediti": "6",
+      "ruolo": "portieri"
+    },
+    {
+      "nome": "Carboni V.*",
+      "crediti": 6,
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Fazzini",
+      "crediti": "1",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Bernardeschi",
+      "crediti": "2",
+      "ruolo": "centrocampisti"
+    },
+    {
+      "nome": "Pedro",
+      "crediti": "1",
+      "ruolo": "attaccanti"
+    }
+  ],
+}
+
+function inserisciDefaultPlayers(giocatoriDiDefault) {
+  btnDefault.classList.add("hidden");
+  btnPrimoSalvataggio.classList.add("hidden");
+  for (const nomeSquadra in giocatoriDiDefault) {
     if (!squadreData[nomeSquadra]) {
       squadreData[nomeSquadra] = {
         portieri: [], difensori: [], centrocampisti: [], attaccanti: [],
@@ -200,7 +489,7 @@ function inserisciDefaultPlayers() {
       };
     }
 
-    defaultPlayers[nomeSquadra].forEach(player => {
+    giocatoriDiDefault[nomeSquadra].forEach(player => {
       squadreData[nomeSquadra][player.ruolo].push({
         nome: player.nome, crediti: player.crediti
       });
@@ -213,7 +502,10 @@ function inserisciDefaultPlayers() {
 
 // --- PULSANTI ---
 const btnDefault = document.getElementById("defaultPlayers");
-btnDefault.addEventListener("click", inserisciDefaultPlayers);
+btnDefault.addEventListener("click", () => {
+  inserisciDefaultPlayers(defaultPlayers);
+});
+
 
 const btnSave = document.getElementById("salvataggio");
 btnSave.addEventListener("click", salvaGiocatori);
@@ -230,6 +522,12 @@ function salvaGiocatori() {
   }
   console.log(JSON.stringify(output, null, 2));
 }
+
+const btnPrimoSalvataggio = document.getElementById("saveOne");
+btnPrimoSalvataggio.addEventListener("click", () => {
+  inserisciDefaultPlayers(primoSave);
+});
+
 
 // --- CHECK ORIENTATION ---
 function checkOrientation() {
